017710674b65e3966d0cb1dec89f4cd9fbe1051d
Varinder Singh <sunny995715@gmail.com>
2022-08-25 05:36:57 +0530

    frontend

---
+++ Auth-Front/components/SignIn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+    <h2 class="text-2xl text-center leading-7 font-semibold">
+      Fill In Your Details To Sign In
+    </h2>
+    <form method="post" name="signinform">
+      <div class="form-group mt-2">
+        <label class="w-full text-center" for="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          class="form-control w-full border-2 border-slate-500 mt-2 px-4 py-2"
+          placeholder="Enter email"
+          value=""
+        >
+      </div>
+      <div class="form-group mt-2">
+        <label class="w-full text-center" for="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          class="form-control w-full border-2 border-slate-500 mt-2 px-4 py-2"
+          placeholder="*********"
+          value=""
+        >
+      </div>
+      <div class="flex justify-center w-full mt-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Sign In
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SignIn',
+  data () {
+    return {}
+  }
+}
+</script>
